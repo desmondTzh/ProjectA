65ba47d454deab601e56670ddb56f3b8cb0571e8
Desmond Tan <46779933+desmondTzh@users.noreply.github.com>
2025-04-30 13:16:26 +0800

    Create test-python_simple.py

---
+++ test-python_simple.py
@@ -0,0 +1,11 @@
+from prefect import flow
+import datetime
+
+@flow(log_prints=True)
+def my_flow():
+    try:
+    
+        current_time = datetime.datetime.now()
+        print(f"THE TIME IS {current_time.strftime("%H:%M:%S")}")
+    except:
+        print("ERROR")
